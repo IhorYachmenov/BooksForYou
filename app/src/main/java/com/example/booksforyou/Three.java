5bfd1266c9f6a29faea1a79d51a67bf1b92ed353
IhorYachmenov <IlLoganlI@Hotmail.com>
2020-03-01 01:33:54 +0200

    create fragment adapter

---
+++ app/src/main/java/com/example/booksforyou/Three.java
@@ -0,0 +1,25 @@
+package com.example.booksforyou;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Three extends Fragment {
+
+    @Override
+     public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.three);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
